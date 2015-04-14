89a5ab20b956dcf865ba46f432890c967ad4d179
walt <912788137@qq.com>
2015-04-14 12:44:00 +0800

    add package

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 /** @jsx React.DOM */
 
+var ChatApp = require('./components/ChatApp.react');
+var ChatExampleData = require('./ChatExampleData');
+var ChatWebAPIUtils = require('./utils/ChatWebAPIUtils');
 var React = require('react');
+window.React = React; // export for http://fb.me/react-devtools
 
+ChatExampleData.init(); // load example data into localstorage
+
+ChatWebAPIUtils.getAllMessages();
+
+React.render(
+    <ChatApp />,
+    document.getElementById('react')
+);
