765464142ca116a89d5c997447f731c74f410496
walt <912788137@qq.com>
2015-04-15 13:01:43 +0800

    fix

--- todo2/constants/AppConstants.js
+++ todo2/constants/AppConstants.js
@@ -2,7 +2,7 @@
  * TodoConstants
  */
 
-var keyMirror = require('keyMirror');
+var React = require("react")
 
 // Constructs an enumeration with keys equal to their value.
 // 也就是讓 hash 的 key 與 value 值一樣
